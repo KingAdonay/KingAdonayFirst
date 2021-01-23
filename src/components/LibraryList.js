53d1b3cfbd8921580e173b2aa96c430660ca9519
adonay eshetu <eshetuadonayad@gmail.com>
2021-01-23 16:23:08 +0300

    initial commit

---
+++ src/components/LibraryList.js
@@ -0,0 +1,28 @@
+import React, {Component } from 'react';
+import { FlatList, View } from 'react-native';
+import { connect } from 'react-redux';
+import ListItem from './ListItem';
+
+class LibraryList extends Component {
+
+    renderItem(library){ 
+        return <ListItem library={library}/>;
+    }
+    
+
+    render(){
+        return (
+            <FlatList
+                data={this.props.libraries}
+                renderItem={this.renderItem}
+                keyExtractor={item=>item.id.toString()}
+            />
+        );
+    }
+}
+
+const mapStateToProps= state=>{
+    return { libraries: state.libraries};
+};
+
+export default connect(mapStateToProps)(LibraryList);
